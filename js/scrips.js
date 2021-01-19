e8262b39323753333184e17d006d3281b5e21ae2
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-19 13:49:41 -0800

    turn the sentence into an array, loop it, reverse the order, add a button Co-authored by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- js/scrips.js
+++ js/scrips.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  $("wordPlay").submit(function(event) {
+  $("#wordPlay").submit(function(event) {
   event.preventDefault();
-  const sentence = $("input#sentence").val();
+  
+  let sentence = $("input#sentence").val();
 
-
-
-  const words = sentence.split(' ');
+  let words = sentence.split(' ');
   let wordlist = [];
-  words.forEach(function(word) {
-    if (word.length > 3) {
+  words.forEach(function (word) {
+    if (word.length >= 3) {
       wordlist.push(word);
-    }
-  });
+     }
+    });
   wordlist.reverse();
-  alert(wordlist.join());
+  let output = wordlist.join(" & ");
+  alert(output);
   });
 });
-
